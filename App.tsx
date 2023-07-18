d0be0a91efbbb53496e4549eb7f32bf041e79691
2019MM607 <jose.martinez19@catolica.edu.sv>
2023-07-18 09:02:15 -0600

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NativeBaseProvider } from "native-base";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <NativeBaseProvider>
+      <Button colorScheme="blue">Button</Button>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
