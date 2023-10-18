5333d9cb9581b1ded7eb43341767a5e231a47ddb
JulioAguilar2019 <53497579+JulioAguilar2019@users.noreply.github.com>
2023-10-18 17:03:51 -0600

    fix(dependencias-fix): dependencies fixed and charBar added

--- src/recognitions/components/RecognitionChartBar.tsx
+++ src/recognitions/components/RecognitionChartBar.tsx
@@ -1,20 +1,18 @@
-import { Center, ScrollView, Spinner } from "native-base";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { useFindByRecognitionsCategoryQuery } from "../../redux/services/recognitions.service";
-import { RootState, useAppSelector } from "../../redux/store/store";
+import React from 'react';
+import { Dimensions } from 'react-native';
+import { BarChart } from "react-native-gifted-charts";
+import { Center, Spinner, Text } from "native-base";
 import Layout from "../../shared/layout/Layout";
+import useTransformedData from '../../hooks/useTransformedData';
+
+const { width } = Dimensions.get('window');
 
 const RecognitionChartBar = () => {
-  const { user } = useAppSelector((state: RootState) => state.user);
-  const {
-    data: recognitionHistory,
-    isError,
-    isLoading,
-    error,
-  } = useFindByRecognitionsCategoryQuery({ id: user?.id as string });
+  const { transformedData, noOfSections, maxValue } = useTransformedData();
 
-  if (isLoading) {
+  const stepValue = Math.max(maxValue / noOfSections, 1);
+
+  if (transformedData.length === 0) {
     return (
       <Center flex={1}>
         <Spinner />
@@ -22,70 +20,20 @@ const RecognitionChartBar = () => {
     );
   }
 
-  if (isError || !recognitionHistory) {
-    return <Text>Error al cargar la data.</Text>;
-  }
-  const maxValue = 10;
-
   return (
     <Layout showCredits={false}>
-      <ScrollView>
-        <View style={{ padding: 20 }}>
-          {recognitionHistory.map((item, index) => {
-            const category = item.category.name;
-            const value = item.total;
-
-            return (
-              <View key={index} style={styles.itemContainer}>
-                <Text style={{ fontSize: 18 }}>{category}</Text>
-                <View style={styles.progressBarContainer}>
-                  <View
-                    style={{
-                      ...styles.progressBar,
-                      width: `${(value / maxValue) * 100}%`,
-                    }}
-                  />
-                  <View style={styles.labelContainer}>
-                    <Text style={styles.labelText}>{value}</Text>
-                  </View>
-                </View>
-              </View>
-            );
-          })}
-        </View>
-      </ScrollView>
+      <BarChart
+        data={transformedData}
+        width={width}
+        horizontal
+        maxValue={maxValue}
+        noOfSections={noOfSections}
+        stepValue={stepValue}
+        disableScroll
+        isAnimated
+      />
     </Layout>
   );
 };
-
-const styles = StyleSheet.create({
-  itemContainer: {
-    backgroundColor: "#f0f0f0",
-    padding: 20,
-    marginBottom: 10,
-    borderRadius: 10,
-  },
-  progressBarContainer: {
-    height: 20,
-    backgroundColor: "#e0e0e0",
-    borderRadius: 10,
-    overflow: "hidden",
-    marginVertical: 10,
-    position: "relative",
-  },
-  progressBar: {
-    height: "100%",
-    backgroundColor: "#000",
-  },
-  labelContainer: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: "center",
-    alignItems: "flex-end",
-  },
-  labelText: {
-    color: "#000",
-    marginRight: 10,
-  },
-});
 
 export default RecognitionChartBar;
