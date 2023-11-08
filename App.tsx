c0347c0a60054e240c9e5a7f89b2d2e6c92e6c9a
JulioAguilar2019 <53497579+JulioAguilar2019@users.noreply.github.com>
2023-11-07 22:59:15 -0600

    feat(dev): Update API URL and key in .env and .env.example files, and add react-native-dotenv plugin to babel.config.js

--- App.tsx
+++ App.tsx
@@ -7,11 +7,13 @@ import Main from './src/Main';
 import { store } from './src/redux/store/store';
 import NoInternetConnection from './src/shared/screens/NoInternetConnection';
 import { theme as CustomTheme } from './src/theme';
+import { API_URL } from "@env"
 
 export default function App() {
      const [isInternetConnected, setIsInternetConnected] = useState(false);
      const theme = extendTheme(CustomTheme);
 
+     console.log(API_URL, "API_URL")
 
      const verifyConnection = async () => {
           Network.getNetworkStateAsync()
